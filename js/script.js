732a112e8089005dc2ec8a604928eb629963386f
Greice Mara <sgreicemaraj@gmail.com>
2021-04-26 12:45:27 -0300

    informações gerais + estilização

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/*function buttonFunction() {
+    document.getElementById("myDropside").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('dropside')) {
+        var dropsides = document.getElementsByClassName("dropsideContent");
+        for (var i = 0; i < dropsides.length; i++) {
+            var openDropside = dropsides[i];
+            if (openDropside.classList.contains('show')) {
+                openDropside.classList.remove('show');
+            }
+        }
+    }
+}*/
